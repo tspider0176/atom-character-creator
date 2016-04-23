9c4eb952bb67ded9f810c2a97be870d7187c5524
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-04-24 03:22:06 +0900

    Add class

--- creator.rb
+++ creator.rb
@@ -1,52 +1,70 @@
+# coding: utf-8
+require 'singleton'
+
 class InvalidImageError < StandardError
 end
 
-def isValidImage?(file)
-  case File.extname(file)
-  when '.png', '.jpg', '.gif'
-    true
-  else
-    false
-  end
+class Main
+  include Singleton
+  AtomCharactorCreator.instance.input
 end
 
-begin
-  arg = ARGV[0]
-  file = File.open(arg)
+class AtomCharactorCreator
+  include Singleton
 
-  raise InvalidImageError unless isValidImage?(file)
-rescue TypeError # 引数ミスの補足
-  puts 'Usage: ruby creator.rb [image]'
-rescue Errno::ENOENT => e # ファイルが存在しない場合の補足
-  puts "#{e}"
-rescue InvalidImageError # 対応しない画像形式が入力された時の補足
-  puts 'Invalid Image file.'
-  puts 'Valid type: *.png, *.jpg, *.gif'
-rescue
-  puts 'A fatal error occured during file I/O'
-else
-  puts 'Input charactor name: '
-  char_name = STDIN.gets.chomp
+  def input
+    begin
+      arg = ARGV[0]
+      file = File.open(arg)
 
-  BASE_DIR = Dir.home + "/.atom/packages/atom-pronama-chan/assets"
+      raise InvalidImageError unless isValidImage?(file)
+    rescue TypeError # 引数ミスの補足
+      puts 'Usage: ruby creator.rb [image]'
+    rescue Errno::ENOENT => e # ファイルが存在しない場合の補足
+      puts "#{e}"
+    rescue InvalidImageError # 対応しない画像形式が入力された時の補足
+      puts 'Invalid Image file.'
+      puts 'Valid type: *.png, *.jpg, *.gif'
+    rescue
+      puts 'A fatal error occured during file I/O'
+    else
+      setup(file)
+    end
+  end
 
-  if Dir.exist?("#{BASE_DIR}/#{char_name}") then
-    puts "#{char_name} is already exists."
-  else
-    `mkdir #{BASE_DIR}/#{char_name}`
-    `mkdir #{BASE_DIR}/#{char_name}/image/`
-    `cp #{File.absolute_path(file)} #{BASE_DIR}/#{char_name}/image/`
-    `mkdir #{BASE_DIR}/#{char_name}/voice/`
-    `touch #{BASE_DIR}/#{char_name}/config.json`
+  private
+  def isValidImage?(file)
+    case File.extname(file)
+    when '.png', '.jpg', '.gif'
+      true
+    else
+      false
+    end
+  end
 
-    config_file = File.open("#{BASE_DIR}/#{char_name}/config.json", "w")
+  def setup(file)
+    puts 'Input charactor name: '
+    char_name = STDIN.gets.chomp
 
-    config_file.print("""{
+    BASE_DIR = Dir.home + "/.atom/packages/atom-pronama-chan/assets"
+
+    if Dir.exist?("#{BASE_DIR}/#{char_name}") then
+      puts "#{char_name} is already exists."
+    else
+      `mkdir #{BASE_DIR}/#{char_name}`
+      `mkdir #{BASE_DIR}/#{char_name}/image/`
+      `cp #{File.absolute_path(file)} #{BASE_DIR}/#{char_name}/image/`
+      `mkdir #{BASE_DIR}/#{char_name}/voice/`
+      `touch #{BASE_DIR}/#{char_name}/config.json`
+
+      config_file = File.open("#{BASE_DIR}/#{char_name}/config.json", "w")
+
+      config_file.print("""{
   \"images\": {
     \"background\": \"#{File.basename(file)}\"
   },
   \"startVoice\": {},
   \"timeSignal\": []
 }""")
+    end
   end
-end
